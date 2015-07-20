19b62d2921236527dbca3ee43a6501e558453137
Daniel Neumann <dkneuma@gmail.com>
2015-07-20 13:36:28 -0400

    Added testing framework

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 /**
  * Created by daniel.neumann on 7/20/15.
  */
+var express = require('express');
+
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('hello world');
+});
+
+app.listen(process.env.PORT || 5000);
+
+module.exports = app;
